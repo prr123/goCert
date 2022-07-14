3217b18b70371f4734de8f075de76897e6f5f9c8
prr123 <priemen@gmail.com>
2022-07-14 02:34:35 +0200

    added creation of sub folders

--- certLib/certLib.go
+++ certLib/certLib.go
@@ -28,7 +28,39 @@ func CreCaFolders (path string) (err error) {
     }
 
     // child folders
-    //
+    // certs
+    certPath := path + "/certs"
+    err = os.Mkdir(certPath, os.ModePerm)
+    if err != nil {
+        return fmt.Errorf("could not create cert folder %s: %v", certPath, err)
+    }
+
+    // crl
+    crlPath := path + "/crl"
+    err = os.Mkdir(crlPath, os.ModePerm)
+    if err != nil {
+        return fmt.Errorf("could not create crl folder %s: %v", crlPath, err)
+    }
+
+    // private
+    privPath := path + "/private"
+    err = os.Mkdir(privPath, os.ModePerm)
+    if err != nil {
+        return fmt.Errorf("could not create private folder %s: %v", privPath, err)
+    }
+
+    // files
+    indexFilnam := path + "/index.txt"
+    _, err = os.Create(indexFilnam)
+    if err != nil {
+        return fmt.Errorf("could not create index file %s: %v", indexFilnam, err)
+    }
+
+    serialFilnam := path + "/serial"
+    _, err = os.Create(serialFilnam)
+    if err != nil {
+        return fmt.Errorf("could not create serial file %s: %v", serialFilnam, err)
+    }
 
     return nil
 }
@@ -37,9 +69,12 @@ func DelCaFolders (path string) (err error) {
 
     _, err = os.Stat(path)
     if os.IsNotExist(err) {
-        return fmt.Errorf("root folder: %s already exists!", path)
+        return fmt.Errorf("root folder: %s does not exist!", path)
     }
 
-
+    err = os.RemoveAll(path)
+    if err != nil {
+        return fmt.Errorf("error deleting rootfolder %s: %v", path, err)
+    }
     return nil
 }
